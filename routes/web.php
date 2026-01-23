a2a0f5819de5b70b47fbebec0f310c0b660cc9b8
freecoding-26 <arizaliwan@gmail.com>
2026-01-23 18:42:12 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('admin.dashboard');
+    return view('home');
 });
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('dashboard');
